770e57b2e972a7ce052dfbd904fb04759166d46b
Rahmannugar <cladeadenugar@gmail.com>
2022-06-05 18:18:15 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ 
+const API_URL = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+    return (
+        <div>
+            
+        </div>
   );
 }
-
-export default App;
